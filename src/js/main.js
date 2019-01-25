79cb59bfb0b91ab585ba8796d8562add60f9cf2e
Dylan Kinzer <dylankinzer@Dylans-MacBook-Pro.local>
2019-01-24 18:16:31 -0800

    made changes to javascript

--- src/js/main.js
+++ src/js/main.js
@@ -2,42 +2,56 @@
 const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
 const base = 'https://api.darksky.net/forecast/';
 const key = 'dabb5db3df15680798d5c40bd2024ff4/';
+let longitude = ''; //-118.243683
+let latitude = ''; //34.052235
+
+//Make days and months array
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October','November', 'December'];
 
 //Grab elements from DOM
 const locationButton = document.querySelector('#locationFinder');
 const displayGrid = document.querySelector('#grid');
 const boxes = document.querySelectorAll('.box');
-console.log(boxes);
 
-// Functions
-function findLocation() {
+
+
+/**************************  Functions *******************************/
+function getDeviceLocation() {
 
     if(!navigator.geolocation) {
-       console.log('OOPS');
+       console.log('Not supported by browser');
     }
 
     function success(position) {
-        console.log('yes');
-        let longitude = position.coords.longitude;
-        let latitude = position.coords.latitude;
+        console.log('Success!');
+        longitude = position.coords.longitude;
+        latitude = position.coords.latitude;
         let newUrl = base + key + latitude + ',' + longitude;
-        console.log(newUrl);
-        //Fetch data 
+
+        // Fetch data 
         fetch(corsProxyUrl + newUrl)
             .then(response => response.json())
             .then(data  => {
-                console.log(data);
                 let daily = data.daily.data;
                 daily.forEach(day => {
+                    //get temps and weather summary
                     let highTemp = day.apparentTemperatureHigh; 
                     let lowTemp = day.apparentTemperatureLow;
                     let summary = day.summary;
+                    //get timestamp and convert to date
+                    let timestamp = day.time;
+                    let fullDate = new Date(timestamp * 1000);
+                    let namedDay = days[fullDate.getDay()];
+                    let date = fullDate.getDate();
+                    let month = fullDate.getMonth();
+                    let year = fullDate.getFullYear(0);
                     let div = document.createElement('div');
                     div.innerHTML = `<div class="day">
-                                        <h1> Today </h1>
+                                        <h1> ${namedDay} </h1>
                                      </div>
                                      <div class="info">
-                                        <p class="date">Jan 15, 2018</p>
+                                        <p class="date">${months[month]} ${date}, ${year}</p>
                                         <h1>${highTemp} / ${lowTemp}</h1>
                                         <p>${summary}</p>
                                      </div>`;
@@ -50,9 +64,17 @@ function findLocation() {
 
     function error(e){
         console.log(e);
+        alert('Oops! No location service..Try zipcode feature');
     }
 
     navigator.geolocation.getCurrentPosition(success, error);
 }
 
-locationButton.addEventListener('click', findLocation);
+
+/************************* Event Listeners **************************/
+locationButton.addEventListener('click', getDeviceLocation);
+
+
+window.onload = function() {
+    getDeviceLocation();  
+}
